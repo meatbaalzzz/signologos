07090f1337e0b83b423cc63b1976c3ed42ce716b
Certified Meatball Lover <diosdelmewing666@gmail.com>
2025-08-09 18:35:46 -0500

    Add vite.config.ts 1754782528323

---
+++ vite.config.ts
@@ -0,0 +1,99 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'jsdelivr-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // 1 year
+              }
+            }
+          }
+        ]
+      },
+      manifest: {
+        name: 'SIGNOLOGOS - Traductor de Lengua a Signos',
+        short_name: 'SIGNOLOGOS',
+        description: 'Traductor basado en la web potenciado con IA de lengua a signo',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@stores': path.resolve(__dirname, './src/stores'),
+      '@types': path.resolve(__dirname, './src/types')
+    }
+  },
+  server: {
+    port: 8000,
+    host: true,
+    hmr: {
+      overlay: true
+    }
+  },
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          ml: ['@tensorflow/tfjs', '@mediapipe/tasks-vision'],
+          animations: ['framer-motion', 'gsap', 'lottie-web']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  },
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      '@tensorflow/tfjs',
+      '@mediapipe/tasks-vision',
+      'framer-motion',
+      'gsap'
+    ]
+  },
+  define: {
+    global: 'globalThis'
+  }
+})
